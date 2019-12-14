9f19fc57bed1f059fe5f74c846899dd66a048014
Liusha He <37186080+LiushaHe0317@users.noreply.github.com>
2019-12-14 14:16:15 +0000

    finished all methods except the visualize part

--- pizzerias/pizzerias_search.py
+++ pizzerias/pizzerias_search.py
@@ -4,59 +4,45 @@ import matplotlib.pyplot as plt
 
 
 class PizzeriasSearcher:
-    def __init__(self, n_of_block: int, shop_locations: Sequence):
+    def __init__(self, n_of_block: int, shop_covers: Sequence):
         self.n_of_block = n_of_block
-        self.city_matrix = numpy.zeros([self.n_of_block, self.n_of_block])
-        self.shop_locations = shop_locations
+        self.shop_covers = shop_covers
 
-    def each_shop_map(self, shop_loc: Sequence, n_of_block: int):
+    def each_shop_matrix(self, shop_loc: Sequence):
         """
         This method takes the location of a shop and dimensionality of the city, converts to a 2D ``numpy.ndarray``
             which indicates the whole area a pizzerias shop delivery service can cover.
 
         :param shop_loc: A sequence containing a tuple of two integers which indicate the coordinates on x- and y- axis and
             an integer which indicates the farthest distance a delivery guy can go.
-        :param n_of_block: An integer which indicates the dimensionality of the city.
         :return: A 2D ``numpy.ndarray``.
         """
-        ...
+        (x_initial, y_initial), r = shop_loc
+        matrix = numpy.zeros([self.n_of_block, self.n_of_block])
 
-    @property
-    def no_of_shops(self):
-        return len(self.shop_locations)
+        # convert x, y coordinates
+        x_center = x_initial - 1                # in numpy, x axis = 1
+        y_center = self.n_of_block - y_initial  # in numpy, y axis = 0
 
-    @property
-    def pizzerias_matrix(self):
-        """
-        This property returns a matrix indicating the whole picture of pizzerias delivery services.
-        """
-        p_matrix = self.city_matrix
-        for shop_loc in self.shop_locations:
-            p_matrix += self.each_shop_map(shop_loc, self.n_of_block)
-        return p_matrix
+        # create a list of x or y coordinate which indicates the cells the shop could cover
+        x_list = [x for x in range(x_center-r, x_center+r+1) if x >= 0 and x < self.n_of_block]
+        # y_list = [y for y in range(y_center-r, y_center+r+1) if y >= 0 and y <= n_of_block-1]
 
-    @property
-    def max_num(self):
-        """
-        This property returns the maximum a city block could have.
-        """
-        ...
+        for d1 in x_list:
+            high_bound = y_center + r - numpy.abs(d1 - x_center) + 1
+            low_bound = y_center - r + numpy.abs(d1 - x_center)
+            matrix[low_bound:high_bound, d1] = 1
 
-    @property
-    def best_homw_locations(self):
-        """
+        return matrix
 
-
-        """
-        ...
-
-    def current_location(self, home_loc: Sequence[int, int]):
+    def number_in_location(self, home_loc: Sequence):
         """
         This method takes a tuple of two integers which indicate the coordinate of a given home location.
 
         :param home_loc: A tuple of integers.
+        :return: number of delivery in the current location.
         """
-        ...
+        return self.pizzerias_matrix[self.n_of_block - home_loc[1], home_loc[0] - 1]
 
     def plot_pizzerias_matrix(self, pizzerias_matrix: numpy.ndarray):
         """
@@ -66,8 +52,34 @@ class PizzeriasSearcher:
         """
         ...
 
-    def load(self):
-        ...
+    @property
+    def no_of_pizzeriass(self):
+        """
+        This method returns the total number of shops in the city.
+        """
+        return len(self.shop_covers)
 
-    def save(self):
-        ...
+    @property
+    def pizzerias_matrix(self):
+        """
+        This method returns a matrix indicating the whole picture of pizzerias delivery services.
+        """
+        p_matrix = numpy.zeros([self.n_of_block, self.n_of_block])
+        for shop_loc in self.shop_covers:
+            p_matrix += self.each_shop_matrix(shop_loc)
+        return p_matrix
+
+    @property
+    def maximum_in_the_city(self):
+        """
+        This method returns the maximum a city block could have.
+        """
+        return int(numpy.amax(self.pizzerias_matrix))
+
+    @property
+    def best_home_locations(self):
+        """
+        This method returns a set of all locations with the maximum number of delivery services.
+        """
+        d0, d1 = numpy.where(self.pizzerias_matrix == numpy.amax(self.pizzerias_matrix))
+        return {(x + 1, self.n_of_block - d0[i]) for i, x in enumerate(d1)}
--- test/test_pizzerias_search.py
+++ test/test_pizzerias_search.py
@@ -1,13 +1,39 @@
 import unittest
+import numpy
 from pizzerias import PizzeriasSearcher
 
 
 class PizzeriasSearchTest(unittest.TestCase):
     def setUp(self):
-        self.no_of_block = 25
-        self.shop_locations = [[(5, 5), 8], [(10, 13), 5],
-                               [(7, 19), 6], [(18, 9), 5]]
+        self.no_of_block = 5
+        self.shop_locations = [[(3, 3), 2], [(1, 1), 2]]
         self.searcher = PizzeriasSearcher(self.no_of_block, self.shop_locations)
 
-    def test_each_shop_map(self):
-        res = self.searcher.each_shop_map(self.shop_locations[0], self.no_of_block)
+    def test_no_of_pizzeriass(self):
+        self.assertEqual(len(self.shop_locations), self.searcher.no_of_pizzeriass,
+                         "no_of_pizzeriass results incorrect.")
+
+    def test_each_shop_matrix(self):
+        res = self.searcher.each_shop_matrix(self.shop_locations[0])
+        expected = numpy.array([[0,0,1,0,0],[0,1,1,1,0],[1,1,1,1,1],[0,1,1,1,0],[0,0,1,0,0]])
+        self.assertEqual(expected.tolist(), res.tolist(), "returned matrix for shop [(3, 3), 2] is not current")
+
+        res = self.searcher.each_shop_matrix(self.shop_locations[1])
+        expected = numpy.array([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [1, 1, 0, 0, 0], [1, 1, 1, 0, 0]])
+        self.assertEqual(expected.tolist(), res.tolist(), "returned matrix for shop [(3, 3), 2] is not current")
+
+    def test_properties(self):
+        expected = numpy.array([[0,0,1,0,0], [0,1,1,1,0], [2,1,1,1,1], [1,2,1,1,0], [1,1,2,0,0]])
+
+        self.assertEqual(expected.tolist(), self.searcher.pizzerias_matrix.tolist(),
+                         "the matrix for all cells the pizzerias could cover is in correct")
+
+        self.assertEqual(2, self.searcher.maximum_in_the_city, "the returned maxinum incorrect")
+
+        self.assertEqual({(1,3), (3,1), (2,2)}, self.searcher.best_home_locations,
+                         "returned best location list incorrect")
+
+    def test_number_in_location(self):
+        self.assertEqual(1, self.searcher.number_in_location((1,1)), "number for cell (1,1) incorrect")
+        self.assertEqual(2, self.searcher.number_in_location((1,3)), "number for cell (1,3) incorrect")
+        self.assertEqual(2, self.searcher.number_in_location((3,1)), "number for cell (3,1) incorrect")
